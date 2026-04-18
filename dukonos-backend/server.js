7874eabcc3b06f1d9adcb470fe369f0370ccbffd
nkmfcs <nkmfcs@gmail.com>
2026-04-18 05:15:50 +0500

    Update server.js

--- dukonos-backend/server.js
+++ dukonos-backend/server.js
@@ -16,8 +16,8 @@ app.use(express.json({ limit: '10mb' }));
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: 'localhost',
-  database: 'dukonos_db',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
 });
